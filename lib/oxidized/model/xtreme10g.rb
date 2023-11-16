9c67da5eacb750c6180f8401d0f5d6bcb952db93
Pierre JACQUOT <pierre.jacquot@inria.fr>
2023-11-16 10:39:51 +0100

    Add Xtreme/10G Managed Ethernet Switch/Router driver.

---
+++ lib/oxidized/model/xtreme10g.rb
@@ -0,0 +1,29 @@
+# INSTALLED BY PUPPET - DO NOT EDIT
+#
+
+class Xtreme10G < Oxidized::Model
+  # For Xtreme 10G switches
+  #
+  # Tested with Connecttech XDG201
+  
+  comment '! '
+  prompt /[a-zA-Z\-]+[#] /
+
+  cmd :all do |cfg|
+    cfg.gsub! /\r/, ''
+  end
+
+  cmd 'show version' do |cfg|
+    comment cfg 
+  end
+
+  cmd 'show running-config' do |cfg|
+    cfg
+  end
+
+  cfg :ssh do
+    password /^[a-z]+@[a-zA-Z\-]'s password: /
+    post_login 'terminal length 0'
+    pre_logout 'exit'
+  end
+end
